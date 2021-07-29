6b59e0fbdb1b0197d7c2b42e63c8d6bf4fc10d81
Fogus <mefogus@gmail.com>
2021-07-29 09:18:50 -0400

    some warmup

--- src/bench_update_kvs/core.clj
+++ src/bench_update_kvs/core.clj
@@ -7,7 +7,8 @@
   (format "%.3g" anum))
 
 (defn time-ms [amt afn]
-  (let [start (System/nanoTime)
+  (let [_ (dotimes [_ (/ amt 2)] (afn))
+        start (System/nanoTime)
         _ (dotimes [_ amt] (afn))
         end (System/nanoTime)]
   (/ (- end start) 1000000.0)
@@ -40,7 +41,7 @@
 (defmacro run-benchmark [name amt-per-iter & exprs]
   (let [afn-map (->> exprs shuffle (map (fn [e] [`(quote ~e) `(fn [] ~e)])) (into {}))]
     `(do
-       (println "Benchmark:" ~name (str "(" ~amt-per-iter " iterations)"))
+       (println "Benchmark:" ~name (str "(" ~amt-per-iter " iterations, with half that amount warmup)"))
        (compare-benchmark ~amt-per-iter ~afn-map)
        (println "\n********************************\n")
        )))
@@ -55,25 +56,25 @@
       (run-benchmark "transform keys/vals of a small map" iterations
                      (update-keys0  inc data)
                      (update-vals0  inc data)
-                     (update-keys1 inc data)
-                     (update-vals1 inc data)
-                     (update-keys2 inc data)
-                     (update-vals2 inc data)
-                     (update-keys3 inc data)
-                     (update-vals3 inc data))
+                     (update-keys-rkv inc data)
+                     (update-vals-rkv inc data)
+                     (update-keys-itr inc data)
+                     (update-vals-itr inc data)
+                     (update-keys-trns inc data)
+                     (update-vals-trns inc data))
 
       (let [data (->> (for [i (range size)] [i i]) (into {}))]
         (run-benchmark "transform values of large map" (/ iterations size)
                        (update-keys0  inc data)
                        (update-vals0  inc data)
-                       (update-keys1 inc data)
-                       (update-vals1 inc data)
-                       (update-keys2 inc data)
-                       (update-vals2 inc data)
-                       (update-keys3 inc data)
-                       (update-vals3 inc data))))
-  (doseq [fun '[update-keys0 update-vals0 update-keys1 update-vals1 update-keys2 update-vals2 update-keys3 update-vals3]
-          m    [(hash-map 0 1 2 3) (array-map 0 1 2 3) #_(sorted-map 2 3 0 1)]]
+                       (update-keys-rkv inc data)
+                       (update-vals-rkv inc data)
+                       (update-keys-itr inc data)
+                       (update-vals-itr inc data)
+                       (update-keys-trns inc data)
+                       (update-vals-trns inc data))))
+  (doseq [fun '[update-keys0 update-vals0 update-keys-rkv update-vals-rkv update-keys-itr update-vals-itr update-keys-trns update-vals-trns]
+          m    [(hash-map 0 1 2 3) (array-map 0 1 2 3) (sorted-map 2 3 0 1)]]
     (let [f (resolve fun)
           r (f inc m)]
       (if (= (type m) (type r))
@@ -82,7 +83,7 @@
 )
 
 (comment
-  (resolve 'update-keys0)
+  (type (first (sorted-map 2 3 0 1)))
   
   (update-keys0 name {:a 1 :b 2})
   (update-vals0 inc {:a 1 :b 2})
