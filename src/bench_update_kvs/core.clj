e191cf5245ed3b7387cdc44348ff8f4f15b4ad25
Fogus <mefogus@gmail.com>
2021-08-05 13:21:51 -0400

    pulled update funs locally

--- src/bench_update_kvs/core.clj
+++ src/bench_update_kvs/core.clj
@@ -3,6 +3,86 @@
             clojure.data.int-map
             clojure.data.avl))
 
+(defn update-keys0
+  "m f => {(f k) v ...}
+
+  Given a map m and a function f of 1-argument, returns a new map whose
+  keys are the result of applying f to the keys of m, mapped to the
+  corresponding values of m.
+  f must return a unique key for each key of m."
+  {:added "1.11"}
+  [m f]
+  (let [ret (with-meta
+              (zipmap (map f (keys m)) (vals m))
+              (meta m))]
+    (if (= (count m) (count ret))
+      ret
+      (throw (RuntimeException. "Key transform function did not return unique values.")))))
+
+(defn update-vals0
+  "m f => {k (f v) ...}
+
+  Given a map m and a function f of 1-argument, returns a new map where the keys of m
+  are mapped to result of applying f to the corresponding values of m."
+  {:added "1.11"}
+  [m f]
+  (with-meta
+    (zipmap (keys m) (map f (vals m)))
+    (meta m)))
+
+(defn update-keys-red
+  "Transient, reduce Map.Entry version"
+  {:added "1.11"}
+  [m f]
+  (let [ret (into (with-meta {} (meta m))
+                  (reduce
+                   (fn [acc ^java.util.Map$Entry me] (conj acc [(f (.getKey me)) (.getValue me)]))
+                   []
+                   m))]
+    (if (= (count m) (count ret))
+      ret
+      (throw (RuntimeException. "Key transform function did not return unique values.")))))
+
+(defn update-vals-red
+  "Transient, reduce Map.Entry version"
+  {:added "1.11"}
+  [m f]
+  (into (with-meta {} (meta m))
+   (reduce
+    (fn [acc ^java.util.Map$Entry me] (update acc (.getKey me) f))
+    m
+    m)))
+
+(defn update-vals-rkv
+  "reduce-kv"
+  {:added "1.11"}
+  [m f]
+  (reduce-kv (fn [acc k _] (update acc k f))
+             m
+             m))
+
+(defn update-keys-trns
+  "Transducer version"
+  {:added "1.11"}
+  [m f]
+    (let [ret (into (with-meta {} (meta m))
+                    (map (fn [[k v]] [(f k) v]))
+                    m)]
+    (if (= (count m) (count ret))
+      ret
+      (throw (RuntimeException. "Key transform function did not return unique values.")))))
+
+(defn update-vals-trns
+  "Transducer version"
+  {:added "1.11"}
+  [m f]
+  (into (with-meta {} (meta m))
+        (map (fn [[k v]] [k (f v)]))
+        m))
+
+
+;;; BENCH STUFF
+
 (defn pretty-float5 [anum]
   (format "%.5g" anum))
 
@@ -96,12 +176,11 @@
                      (update-vals-rkv data inc)
                      (update-vals-trns data inc))))
   
-  (doseq [fun '[update-keys0 update-vals0 update-keys-red update-vals-red update-vals-rkv update-keys-trns update-vals-trns]
+  (doseq [fun [update-keys0 update-vals0 update-keys-red update-vals-red update-vals-rkv update-keys-trns update-vals-trns]
           m    [(hash-map 0 1 2 3) (array-map 0 1 2 3) (sorted-map 2 3 0 1)
                 (clojure.data.priority-map/priority-map 0 1 2 3)
                 (clojure.data.int-map/int-map (int 0) (int 1) (int 2) (int 3))
                 (clojure.data.avl/sorted-map 0 1 2 3)]]
     (println "Checking type " (type m) " against " fun)
-    (let [f (resolve fun)]
-      (f m inc))))
+    (fun m inc)))
 
