c62075054467492f614cbd2445b04a653f3a5381
Fogus <mefogus@gmail.com>
2021-08-26 16:00:54 -0400

    remove kv-red2

--- src/bench_update_kvs/disp.clj
+++ src/bench_update_kvs/disp.clj
@@ -3,19 +3,6 @@
 
 (set! *warn-on-reflection* true)
 
-(defn reduce-kv2
-  "Reduces an associative collection. f should be a function of 3
-  arguments. Returns the result of applying f to init, the first key
-  and the first value in coll, then applying f to that result and the
-  2nd key and value, etc. If coll contains no entries, returns init
-  and f is not called. Note that reduce-kv is supported on vectors,
-  where the keys will be the ordinals."  
-  {:added "1.4"}
-  ([f init coll]
-   (if (instance? clojure.lang.IKVReduce coll)
-     (.kvreduce ^clojure.lang.IKVReduce coll f init)
-     (clojure.core.protocols/kv-reduce coll f init))))
-
 (extend-protocol clojure.core.protocols/IKVReduce
   nil
   (kv-reduce
